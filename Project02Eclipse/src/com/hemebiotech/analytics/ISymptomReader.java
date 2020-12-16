afcff8e4bfd86d53fb12d41118bac7ac8481c1c0
Boniface Eric <boniric45@gmail.com>
2020-12-16 12:22:38 +0100

    Release initial.

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,20 +1,69 @@
-package com.hemebiotech.analytics;
-
-import java.util.List;
-
-/**
+ /*
+ * AnalyticsCounter class
+ * 
+ * Description : 
+ * 
  * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
  * that may contain many duplications
- * 
  * The implementation does not need to order the list
  * 
+ * Version : 1.0 
+ * 
+ * Date : 10/12/2020
+ * 
+ * Copyright : Eric Boniface
+ * 
  */
+
+package com.hemebiotech.analytics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+
 public interface ISymptomReader {
 	/**
-	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 * Retrieving information from the ReadSymptomDataFromFile class
+	 * and return listrelease to the main class
+	 * @version 1.0
+	 * @author Eric
 	 */
-	List<String> GetSymptoms ();
+
+		
+	public static List<String> GetSymptoms () throws IOException {
+
+		/* results of ReadSymptomDataFromFile */
+	ArrayList<String> symptomResult = (ArrayList<String>) ReadSymptomDataFromFile.GetSymptoms();
+
+  	
+		/* Collection creation to avoid duplicates */
+	Collection<String> resultList = new HashSet<String>(); 
+
+    for(String symptom : symptomResult){	
+     
+		
+    	/* count the number of symptoms */
+	int symptomCount = Collections.frequency(symptomResult, symptom); 
+	
+		/* adds the symptom with its number in the resultList collection */
+	resultList.add(symptom + " : " + symptomCount);  
+	//System.out.println(" SymptomReader> "+resultList);
+	
+     }
+    	/* Creation of a List in order to be able to classify it in alphabetical order */
+    ArrayList<String> listRelease = new ArrayList<String>(resultList); 
+    
+    	/* alphabetical order */
+    Collections.sort(listRelease); 
+
+
+ 	return listRelease;
+ 	
+	}
+	
 }
+
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,3 +1,18 @@
+/*
+ * AnalyticsCounter class
+ * 
+ * Description : 
+ * 
+ * Read the symptom.txt file and return the result to the ISymptomReader class
+ * 
+ * Version : 1.0 
+ * 
+ * Date : 10/12/2020
+ * 
+ * Copyright : Eric Boniface
+ * 
+ */
+
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
@@ -6,42 +21,63 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Simple brute force implementation
- *
- */
-public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
+public class ReadSymptomDataFromFile implements ISymptomReader {
 	
+	private static String filepath="symptoms.txt";       // file path 
+
 	/**
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @version 1.0
+	 * @author Eric
+	 * 
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
+		
 		this.filepath = filepath;
+		
 	}
 	
-	@Override
-	public List<String> GetSymptoms() {
+	/**
+	 * 
+	 * read the file and add the symptoms one per line to the result list
+	 * @return result
+	 * @exception e
+	 * @author Eric
+	 * 
+	 */
+	public static List<String> GetSymptoms() {
+		
 		ArrayList<String> result = new ArrayList<String>();
 		
-		if (filepath != null) {
+			if (filepath != null) {
+			
 			try {
+				
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				
 				String line = reader.readLine();
 				
 				while (line != null) {
+					
 					result.add(line);
-					line = reader.readLine();
-				}
+					line = reader.readLine();	
+					
+					}
+				
 				reader.close();
+				
+				
 			} catch (IOException e) {
+				
 				e.printStackTrace();
 			}
 		}
-		
-		return result;
+		  	//System.out.println("Read > "+result);
+		  	
+	return result;
+
 	}
 
 }
